737cfab95f2499aae26f32c779a8f8e74d252c2c
Declan <90400076+d3tk@users.noreply.github.com>
2022-02-17 16:11:21 -0500

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,4 @@
-# Implementation of Two Player Tic-Tac-Toe game in Python.
-
-''' We will make the board using dictionary 
-    in which keys will be the location(i.e : top-left,mid-right,etc.)
-    and initialliy it's values will be empty space and then after every move 
-    we will change the value according to player's choice of move. '''
-
+# intialize the board
 theBoard = {'7': ' ', '8': ' ', '9': ' ',
             '4': ' ', '5': ' ', '6': ' ',
             '1': ' ', '2': ' ', '3': ' '}
@@ -14,10 +8,6 @@ board_keys = []
 for key in theBoard:
     board_keys.append(key)
 
-''' We will have to print the updated board after every move in the game and 
-    thus we will make a function in which we'll define the print_board function
-    so that we can easily print the board everytime by calling this function. '''
-
 
 def print_board(board):
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
@@ -25,8 +15,6 @@ def print_board(board):
     print(board['4'] + '|' + board['5'] + '|' + board['6'])
     print('-+-+-')
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
-
-# Now we'll write the main function which has all the gameplay functionality.
 
 
 def game():
@@ -47,7 +35,6 @@ def game():
             print("That place is already filled.\nMove to which place?")
             continue
 
-        # Now we will check if player X or O has won,for every move after 5 moves.
         if count >= 5:
             if theBoard['7'] == theBoard['8'] == theBoard['9'] != ' ':  # across the top
                 print_board(theBoard)
@@ -90,18 +77,15 @@ def game():
                 print(" **** " + turn + " won. ****")
                 break
 
-        # If neither X nor O wins and the board is full, we'll declare the result as 'tie'.
         if count == 9:
             print("\nGame Over.\n")
             print("It's a Tie!!")
 
-        # Now we have to change the player after every move.
         if turn == 'X':
             turn = 'O'
         else:
             turn = 'X'
 
-    # Now we will ask if player wants to restart the game or not.
     restart = input("Do want to play Again?(y/n)")
     if restart == "y" or restart == "Y":
         for key in board_keys:
